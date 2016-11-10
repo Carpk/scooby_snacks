ad3db9095e87f5f67f583b923e3cd79b1076a61c
brechen <zappeln@gmail.com>
2016-11-09 18:11:15 -0600

    using spree assest pipeline to replace default pictures

--- app/overrides/replace_default_logo.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(:virtual_path  => "spree/shared/_header",
-                     :name          => "replace default logo",
-                     :replace       => "figure#logo",) do
-  '<figure id="logo" class="col-md-4 col-sm-3" data-hook>
-    <img src="assets/logo.png">
-  </figure>'
-end
